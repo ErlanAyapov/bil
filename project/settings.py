564987ec2cbc28be576c9089db5acdbf5451e967
Yerlan <www.aiapov@gmail.com>
2025-01-03 01:23:18 +0500

    Fixed error

--- project/settings.py
+++ project/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.environ.get('SECRET_KEY'))
-
-print(SECRET_KEY)
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.123.142.105', '127.0.0.1', 'localhost']
 
 
 # Application definition
