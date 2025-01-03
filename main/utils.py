38b816b66b9c79a562eb1e17ab0fcb8f725032bb
Yerlan <www.aiapov@gmail.com>
2025-01-03 10:31:55 +0500

    Config push notifations

---
+++ main/utils.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+# Инициализация Firebase
+cred = credentials.Certificate("../bil7-dfc73-firebase-adminsdk-azxfl-574a75e22d.json")
+firebase_admin.initialize_app(cred)
+
+# Функция для отправки уведомлений
+def send_notification(registration_token, title, body):
+    # Создаем сообщение
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body,
+        ),
+        token=registration_token,  # Токен устройства
+    )
+
+    # Отправляем сообщение
+    response = messaging.send(message)
+    print('Successfully sent message:', response)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from utils import send_notification
+
 
 # Create your views here.
 def main(request):
+    
     return render(request, 'main/main.html')
